4b5138348b020df0c9c3970486acca1411ed14fe
ksaisaranya1102 <139947328+ksaisaranya1102@users.noreply.github.com>
2023-11-27 21:51:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,8 @@
 
 exec python3 app.py &
 exec python3 gradio/demo.py
+
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx libsm6 libxrender1 libxext6
+
+pip install --upgrade opencv-python
